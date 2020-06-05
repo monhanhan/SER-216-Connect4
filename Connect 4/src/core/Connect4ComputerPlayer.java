acbadceaa53f9a7f873228402baaff9d5f7d0aa9
Ryan <Ryan@Alfred>
2020-06-05 16:03:21 -0700

    Comments added.

--- Connect 4/src/core/Connect4ComputerPlayer.java
+++ Connect 4/src/core/Connect4ComputerPlayer.java
@@ -1,6 +1,22 @@
 package core;
 
+/**
+ * 
+ * @author Ryan Munin
+ * @version 1.0 This class controls the logic for a computer controlled player
+ *          to play connect 4.
+ *
+ */
 public class Connect4ComputerPlayer {
+    /**
+     * This is the logic for a computer player to place their pieces. It places in
+     * the leftmost column until that column is full, then moves on to the next
+     * column.
+     * 
+     * @param myGame is a connect4 game object.
+     * @param player is a character representing which player the computer is
+     *               playing as. X or O.
+     */
     public static void takeTurn(Connect4 myGame, char player) {
 	boolean validMove = false;
 	int currCol = 0;
