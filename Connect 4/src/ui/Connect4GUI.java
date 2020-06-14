0e693fe688feb33713142a61fc4d6f82a9334b23
Ryan <Ryan@Alfred>
2020-06-14 16:41:54 -0700

    Attributes set up.

--- Connect 4/src/ui/Connect4GUI.java
+++ Connect 4/src/ui/Connect4GUI.java
@@ -1,8 +1,24 @@
 package ui;
 
 import core.Connect4;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Connect4GUI {
+public class Connect4GUI extends Application {
     private Connect4 myBoard;
+    private Stage primaryStage;
+    private boolean computerPlayer;
+
+    // TODO: these seem arbitrary. Be sure to check that these actually make sense
+    // in practice.
+    private final int PANESIZE = 384;
+    private final int INSET = 8;
+    private final int TILESIZE = 46;
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+	// TODO Auto-generated method stub
+
+    }
 
 }
